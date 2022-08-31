ccd2b412bccd0a68423de9c5e74b26c8a1c42b3a
sudha-guragol <110382111+sudha-guragol@users.noreply.github.com>
2022-08-31 16:16:16 +0530

    Admin-Client-1

---
+++ 60-SB-Admin-Client-App-1/src/main/java/in/ashokit/rest/MsgRestController.java
@@ -0,0 +1,15 @@
+package in.ashokit.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MsgRestController {
+@GetMapping("/hello")
+	public String getMsg()
+	{
+		return "Hello";
+	}
+	
+	
+}
