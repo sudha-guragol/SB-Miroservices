8ee9d092ce2a091ea6b2fe6718ffd1c779344c7c
sudha-guragol <110382111+sudha-guragol@users.noreply.github.com>
2022-08-31 15:59:41 +0530

    SB Service Registry Ex

---
+++ 50-SB_MS-Service-Registry/src/main/java/in/ashokit/Application.java
@@ -0,0 +1,16 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+//enable eureka server in our application
+@EnableEurekaServer
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
